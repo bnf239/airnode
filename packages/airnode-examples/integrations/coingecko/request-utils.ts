863f7c5aabc0a6773b64240fc9603a0046b4783f
bnf239 <47250770+bnf239@users.noreply.github.com>
2021-12-14 22:44:19 -0500

    Add files via upload

--- packages/airnode-examples/integrations/coingecko/request-utils.ts
+++ packages/airnode-examples/integrations/coingecko/request-utils.ts
@@ -1,11 +1,10 @@
 import { encode } from '@api3/airnode-abi';
 import { cliPrint, getDeployedContract, readIntegrationInfo } from '../../src';
 
-const coinLabel = 'Ethereum';
-const coinId = coinLabel.toLowerCase();
+const res = 'result';
 
 export const getEncodedParameters = () => {
-  return encode([{ name: 'coinId', type: 'bytes32', value: coinId }]);
+  return encode([{ name: 'result', type: 'bytes32', value: res }]);
 };
 
 export const printResponse = async (requestId: string) => {
@@ -13,5 +12,5 @@ export const printResponse = async (requestId: string) => {
   const requester = await getDeployedContract(`contracts/${integrationInfo.integration}/Requester.sol`);
 
   // Divided by 1e6, because the response value is multiplied with 1e6 by Airnode
-  cliPrint.info(`${coinLabel} price is ${(await requester.fulfilledData(requestId)) / 1e6} USD`);
+  cliPrint.info(`${res}  is ${(await requester.fulfilledData(requestId))} `);
 };
